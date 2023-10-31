7c0a734aaf239d7be0b652ade7dff5a466e89d5c
NickName2719 <146065137+NickName2719@users.noreply.github.com>
2023-10-31 15:07:23 +0800

    Update hw5-1.c

--- hw5-1.c
+++ hw5-1.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main ( ) {
+    float c ;
+    scanf ( "%f" , &c ) ;
+    double f = c * 9 / 5 + 32 ;
+    printf ( "%.1f\n" , f ) ;
+    return 0 ;
+}
