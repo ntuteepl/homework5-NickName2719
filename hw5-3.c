7d2512878d0bbd0e8592a179adac71b78a75ebef
NickName2719 <146065137+NickName2719@users.noreply.github.com>
2023-10-31 15:07:45 +0800

    Update hw5-3.c

--- hw5-3.c
+++ hw5-3.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main ( ) {
+    int time , paid ;
+    double out ;
+    scanf( "%d%d" , &time , &paid );
+    if ( time <= 60 ) {
+        out = paid * time ;
+    }
+    else if ( time
+              <= 120 ) {
+        out = paid * 60 + 1.33 * paid * (time-60) ;
+    }
+    else {
+        out = paid * 60 + 1.33 * paid * 60 + 1.66 * paid * (time-120) ;
+    }
+    printf( "%.1f\n" , out );
+    return 0;
+}
