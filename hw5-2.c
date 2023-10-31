4fdb30bf3e5ba0eee7609a13f73165f90800b5e3
NickName2719 <146065137+NickName2719@users.noreply.github.com>
2023-10-31 15:07:34 +0800

    Update hw5-2.c

--- hw5-2.c
+++ hw5-2.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main ( ) {
+    int in ;
+    double out ;
+    scanf( "%d" , &in );
+    if ( in <= 800 ) {
+        out = 0.9 * in ;
+    }
+    else if ( in <= 1500 ) {
+        out = 0.9 * 0.9 * in ;
+    }
+    else {
+        out = 0.79 * 0.9 * in ;
+    }
+    printf( "%.1f\n" , out );
+    return 0;
+}
